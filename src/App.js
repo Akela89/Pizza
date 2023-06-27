ff5a95bca55c774fccde8f79adc3bee7399e8a6d
Ilya Soloviev <ilya0960@gmail.com>
2023-06-27 18:11:48 +0300

    added reactSkeletonLoader

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Sort from './Components/Sort';
 import PizzaBlock from './Components/PizzaBlock';
 import Categories from './Components/Categories';
 import pizzas from './assets/pizzas.json';
+import { Skeleton } from './Components/PizzaBlock/Skeleton';
 
 console.log(pizzas);
 
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch('https://649ad76dbf7c145d0239920e.mockapi.io/items')
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
+        setIsLoading(false);
       });
   }, []);
 
@@ -30,9 +33,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
