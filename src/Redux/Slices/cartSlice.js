e2fb9f27ec09c2562a8041205b5ef36acc0a1f84
Ilya Soloviev <ilya0960@gmail.com>
2023-07-25 16:13:34 +0300

    fix: fixed bugs

--- src/Redux/Slices/cartSlice.js
+++ src/Redux/Slices/cartSlice.js
@@ -10,12 +10,6 @@ const cartSlice = createSlice({
   initialState,
 
   reducers: {
-    // addItem(state, action) {
-    //   state.items.push(action.payload);
-    //   state.totalPrice = state.items.reduce((sum, obj) => {
-    //     return obj.price + sum;
-    //   }, 0);
-    // },
     addItem(state, action) {
       const findItem = state.items.find((obj) => obj.id === action.payload.id);
       if (findItem) {
@@ -31,13 +25,23 @@ const cartSlice = createSlice({
         return obj.price * obj.count + sum;
       }, 0);
     },
-    removeItem(state, action) {
-      state.items = state.items.filter((obj) => obj.id === action.payload);
+
+    minusItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload);
+      if (findItem) {
+        findItem.count--;
+      }
     },
+
+    removeItem(state, action) {
+      state.items = state.items.filter((obj) => obj.id !== action.payload);
+    },
+
     clearItem(state) {
       state.items = [];
+      state.totalPrice = 0;
     },
   },
 });
-export const { addItem, removeItem, clearItem } = cartSlice.actions;
+export const { addItem, removeItem, minusItem, clearItem } = cartSlice.actions;
 export default cartSlice.reducer;
